588405cde3729e2d24e1b6bffe7cb79a8470d910
glen-s-abraham <glenprojects666@gmail.com>
2021-06-17 11:56:46 +0530

    Pagination and Caching

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,9 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Cache;
 
 trait ApiResponser{
 	protected function successResponse($data,$code)
@@ -27,7 +30,9 @@ trait ApiResponser{
 		$transformer=$collection->first()->transformer;
 		$collection=$this->filterData($collection,$transformer);
 		$collection=$this->sortData($collection,$transformer);
+		$collection=$this->paginate($collection);
 		$collection=$this->transformData($collection,$transformer);
+		$collection=$this->cacheResponse($collection);
 		return $this->successResponse(["data"=>$collection],$code);
 	}
 
@@ -65,6 +70,38 @@ trait ApiResponser{
 	{
 		return fractal($data, new $transformer)->toArray();
 	
+	}
+
+	protected function paginate(Collection $collection)
+	{
+		$rules=[
+			'perPage'=>'integer|min:2|max:50'
+		];
+		Validator::validate(request()->all(),$rules);
+		$page=LengthAwarePaginator::resolveCurrentPage();
+		$perPage=15;
+		if(request()->has('perPage'))
+		{
+			$perPage=(int)request()->perPage;
+		}
+		$results=$collection->slice(($page-1)*$perPage,$perPage)->values();
+		$paginated=new LengthAwarePaginator($results,$collection->count(),$perPage,$page,[
+			"path"=>LengthAwarePaginator::resolveCurrentPath(),
+		]);
+		$paginated->append(request()->all());
+		return $paginated;
+	}
+
+	protected function cacheResponse($data)
+	{
+		$url=request()->url();
+		$queryParams=request()->query();
+		ksort($queryParams);
+		$queryString=http_build_query($queryParams);
+		$fullUrl=$url.$queryString;
+		return Cache::remember($fullUrl,30,function() use($data){
+					return $data;
+		});
 	}
 }
 ?>
